a342b2007cf33e3932fe2217b5eb8ff8cc487a97
Ganesh Ugale <ganesh.ugale@fusionstak.com>
2022-11-10 11:35:47 +0530

    added views@

---
+++ TSheetProject/TSheetProject/Controllers/SuperAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSheetProject.Controllers
+{
+    public class SuperAdminController : Controller
+    {
+        // GET: SuperAdmin
+        public ActionResult SuperAdmin()
+        {
+            return View();
+        }
+    }
+}
