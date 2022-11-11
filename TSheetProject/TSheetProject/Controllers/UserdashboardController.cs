ccc527b89f52690c695ebc80a0ba702f6a186ed0
Ganesh Ugale <ganesh.ugale@fusionstak.com>
2022-11-11 11:49:38 +0530

    Merged timesheet-view into development

---
+++ TSheetProject/TSheetProject/Controllers/UserdashboardController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSheetProject.Controllers
+{
+    public class UserdashboardController : Controller
+    {
+        // GET: Userdashboard
+        public ActionResult UserDashboard()
+        {
+            return View();
+        }
+
+        public ActionResult AllTimeSheet()
+        {
+            return View();
+        }
+    }
+}
