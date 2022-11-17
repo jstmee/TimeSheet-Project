2a59ccd6bb72b6f8c298af7e864016f7e8a8092c
samiksha_ghuge <samiksha.ghuge@fusionstak.com>
2022-11-17 18:03:03 +0530

    implemented generic repository

---
+++ TSheetProject/TSheet.IBL/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSheet.IBL
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(Object id);
+        void Add(T obj);
+        void Update(T obj);
+        void Delete(object id);
+        void Save();
+
+
+    }
+}
