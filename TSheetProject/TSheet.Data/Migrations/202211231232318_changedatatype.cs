3e772ca0fcedbe0bf5db376d23392aa173cb04ab
samiksha.ghuge <samiksha.ghuge@fusionstak.com>
2022-11-23 18:19:17 +0530

    added migration and changed many things

---
+++ TSheetProject/TSheet.Data/Migrations/202211231232318_changedatatype.cs
@@ -0,0 +1,18 @@
+﻿namespace TSheet.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changedatatype : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.TimeSheetMaster", "Comment", c => c.String(nullable: false, maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.TimeSheetMaster", "Comment", c => c.Binary(nullable: false, maxLength: 50));
+        }
+    }
+}
---
+++ TSheetProject/TSheet.Modals/ChangeUserPassword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSheet.Models
+{
+    public class ChangeUserPassword
+    {
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirmation Password must match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
