2d6e35f8f7cef8a5f355125e440a80adbc5acc4e
Vishnu Kumar <vishnu.kumar@fusionstak.com>
2022-11-25 18:45:28 +0530

    adding two new modal classes needed for saving user data to database

---
+++ TSheetProject/TSheet.Modals/ProjectLists.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSheet.Modals
+{
+    public class ProjectLists
+    {
+        public int project1 { get; set; }
+
+        public int? project2 { get; set; }
+
+        public int? project3 { get; set; }
+
+        public int? project4 { get; set; }
+
+
+    }
+}
---
+++ TSheetProject/TSheet.Modals/ProjectRow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSheet.Modals
+{
+    public class ProjectRow
+    {
+
+        public int Id { get; set; }
+        public int? day1 { get; set; }
+        public int? day2 { get; set; }
+        public int? day3 { get; set; }
+        public int? day4 { get; set; }
+        public int? day5 { get; set; }
+        public int? day6 { get; set; }
+        public int? day7 { get; set; }
+        public string comment { get; set; }
+    }
+}
